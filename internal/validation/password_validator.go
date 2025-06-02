5904e1b4f621fd577da9c16917d6cfeebda7a6db
CP Payne <charlethical1@gmail.com>
2025-06-02 19:03:28 +0200

    feat: implement custom password validator

---
+++ internal/validation/password_validator.go
@@ -0,0 +1,37 @@
+package validation
+
+import (
+	"unicode"
+
+	"github.com/go-playground/validator/v10"
+)
+
+func validatePasswordComplexity(fl validator.FieldLevel) bool {
+	password := fl.Field().String()
+
+	if len(password) < 8 {
+		return false
+	}
+
+	var (
+		hasUpper   bool
+		hasLower   bool
+		hasNumber  bool
+		hasSpecial bool
+	)
+
+	for _, char := range password {
+		switch {
+		case unicode.IsUpper(char):
+			hasUpper = true
+		case unicode.IsLetter(char):
+			hasLower = true
+		case unicode.IsNumber(char):
+			hasNumber = true
+		case unicode.IsPunct(char) || unicode.IsSymbol(char):
+			hasSpecial = true
+		}
+	}
+
+	return hasUpper && hasLower && hasNumber && hasSpecial
+}
