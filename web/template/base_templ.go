b9b6d4f56b88821bc3c6cfecbb43fe2f7ccf80ff
CP Payne <charlethical1@gmail.com>
2025-06-02 19:02:27 +0200

    style: group imports

--- web/template/base_templ.go
+++ web/template/base_templ.go
@@ -8,8 +8,10 @@ package template
 import "github.com/a-h/templ"
 import templruntime "github.com/a-h/templ/runtime"
 
-import "github.com/CP-Payne/wonderpicai/web/template/components/navigation"
-import "github.com/CP-Payne/wonderpicai/web/template/components/ui"
+import (
+	"github.com/CP-Payne/wonderpicai/web/template/components/navigation"
+	"github.com/CP-Payne/wonderpicai/web/template/components/ui"
+)
 
 func Base(useNav bool) templ.Component {
 	return templruntime.GeneratedTemplate(func(templ_7745c5c3_Input templruntime.GeneratedComponentInput) (templ_7745c5c3_Err error) {
