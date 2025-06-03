52d31ee2f8765bce6d3f8b10e1f789d474db21ce
CP Payne <charlethical1@gmail.com>
2025-06-03 14:17:28 +0200

    feat: implement better error handling and logging

--- internal/service/auth_service.go
+++ internal/service/auth_service.go
@@ -12,43 +12,41 @@ import (
 )
 
 type AuthService interface {
-	Register(username, email, password string) (*domain.User, error)
+	Register(username, email, password string) (*domain.User, string, error)
 	Login(username, password string) (*domain.User, string, error)
 }
 
 type authServiceImpl struct {
-	appLogger *zap.Logger
-	userRepo  port.UserRepository
+	logger   *zap.Logger
+	userRepo port.UserRepository
 }
 
 func NewAuthService(userRepo port.UserRepository /*, tokenService port.TokenService */, logger *zap.Logger) AuthService {
 	return &authServiceImpl{
-		userRepo:  userRepo,
-		appLogger: logger,
+		userRepo: userRepo,
+		logger:   logger.With(zap.String("component", "AuthService")),
 		// tokenService: tokenService
 	}
 }
 
-func (s *authServiceImpl) Register(username, email, password string) (*domain.User, error) {
-
-	if username == "" || email == "" || password == "" {
-		return nil, errors.New("username, email and password are required")
-	}
-
+func (s *authServiceImpl) Register(username, email, password string) (*domain.User, string, error) {
 	existingUser, err := s.userRepo.GetByEmail(email)
-	if err != nil {
-		return nil, fmt.Errorf("error checking for existing user: %w", err)
+	if err != nil && !errors.Is(err, domain.ErrUserNotFound) {
+		s.logger.Error("Error checking existing user by email", zap.Error(err))
+		return nil, "", fmt.Errorf("registration process failed: %w", err)
 	}
+
 	if existingUser != nil {
-		return nil, errors.New("user with this email already exists") // TODO: Create custom domain error domain.ErrUserAlreadyExists
+		return nil, "", domain.ErrEmailAlreadyExists
 	}
 
 	hashedPassword, err := hashPassword(password)
 	if err != nil {
-		return nil, fmt.Errorf("error checking for existing user: %w", err)
+		s.logger.Error("Password hashing failed", zap.Error(err))
+		return nil, "", fmt.Errorf("user creation failed: %w", err)
 	}
 
-	user := &domain.User{
+	userToCreate := &domain.User{
 		BaseModel: domain.BaseModel{
 			ID:        uuid.New(),
 			CreatedAt: time.Now(),
@@ -59,16 +57,22 @@ func (s *authServiceImpl) Register(username, email, password string) (*domain.Us
 		Password: hashedPassword,
 	}
 
-	if err := s.userRepo.Create(user); err != nil {
-		return nil, fmt.Errorf("failed to create user: %w", err)
+	if err := s.userRepo.Create(userToCreate); err != nil {
+		if errors.Is(err, domain.ErrEmailAlreadyExists) || errors.Is(err, domain.ErrDuplicateEntry) {
+			s.logger.Warn("User creation conflict", zap.Error(err))
+			return nil, "", err
+		}
+		s.logger.Error("Failed to create user via repository", zap.Error(err))
+
+		return nil, "", fmt.Errorf("failed to complete registration due to an internal issue: %w", err)
 	}
 
 	// Don't return password, even if it is hashed
-	user.Password = ""
+	userToCreate.Password = ""
 
 	// TODO: Generate token, Update return to also return the token
 
-	return user, nil
+	return userToCreate, "", nil
 }
 
 func (s *authServiceImpl) Login(email, password string) (*domain.User, string, error) {
